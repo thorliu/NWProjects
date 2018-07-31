43cfff8e02d735a92bb21098f4b1285520b42cfc
刘强 <thor.liu@outlook.com>
2018-07-31 17:03:19 +0800

    新增WebClient模块

--- projects/CCFWS/CreatorProject/assets/resources/scripts/fws/FWSEnv.ts
+++ projects/CCFWS/CreatorProject/assets/resources/scripts/fws/FWSEnv.ts
@@ -3,18 +3,25 @@
  * @Author: 刘强 
  * @Date: 2018-07-31 15:20:34 
  * @Last Modified by: 刘强
- * @Last Modified time: 2018-07-31 16:39:15
+ * @Last Modified time: 2018-07-31 16:59:27
  */
 
 module FWSEnv
 {
 	// NOTE: 基础参数
 
-	/** 是否开启调试功能 */
-	export const DEBUG :boolean = CC_DEBUG;
-
 	/** 是否使用生产环境 */
 	export const PROD: boolean = false;
+
+	export const API_BASE_URL:string = PROD ? 
+										"" :
+										"";
+	export const WS_BASE_URL:string = PROD ?
+										"" :
+										"";
+
+	/** 是否开启调试功能 */
+	export const DEBUG :boolean = CC_DEBUG;
 
 	/** 是否模拟器环境 */
 	export const SIMULATOR: boolean = true;
@@ -24,8 +31,6 @@ module FWSEnv
 
 	// NOTE: 高级参数
 
-	
-
 	/** 是否开启高级调试功能 */
 	export const DEBUG_ADVANCED: boolean = DEBUG && SIMULATOR && true;
 
@@ -34,5 +39,8 @@ module FWSEnv
 	
 	/** 是否跟踪CONTEXT开关 */
 	export const DEBUG_CONTEXT_TRACE: boolean = DEBUG && false;
+
+	/** 是否跟踪HTTP请求 */
+	export const DEBUG_WEBCLIENT_TRACE: boolean = DEBUG && false;
 }
 export = FWSEnv;
